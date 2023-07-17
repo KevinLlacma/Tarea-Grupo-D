a6960f458025ff7325683aafd4a18f5b9ab11689
KevinLlacma <kllacma@unsa.edu.pe>
2023-07-17 16:27:13 -0500

    Cambios registro

--- MyDjangoProject/MyApplication/admin.py
+++ MyDjangoProject/MyApplication/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from .models.Assignment import Assignment 
+from .models.Classroom import Classroom 
+from .models.Course import Course 
+from .models.DetailSemester import DetailSemester
+from .models.Enroll import Enroll
+from .models.Group import Group 
+from .models.School import School
+from .models.Semester import Semester
+from .models.User_Type import User_Type
+from .models.User import User  
 # Register your models here.
+
+admin.site.register(Assignment)
+admin.site.register(Classroom)
+admin.site.register(DetailSemester)
+admin.site.register(Course)
+admin.site.register(Enroll)
+admin.site.register(Group)
+admin.site.register(School)
+admin.site.register(Semester)
+admin.site.register(User_Type)
+admin.site.register(User)
